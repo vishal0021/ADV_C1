87e0543b84a8d914d2b4f76b0f846ca2b028faca
Vishal Sharma <33948196+vishal0021@users.noreply.github.com>
2022-08-05 14:22:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var timeStamp = "Fri Aug 05 2022 13:44:51 GMT+0530" //local time
+
+if (isEclipse(timeStamp) == true)
+    {
+        console.log("Satellite is in the eclipse state")
+    }
+
+else
+    {
+        console.log("Satellite is in sun light state")
+    }
+
+
